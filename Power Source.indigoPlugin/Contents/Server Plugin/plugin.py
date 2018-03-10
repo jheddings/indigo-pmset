33a82a921dd0e031b867d6af7302777eed2fd826
jheddings <jason@heddings.com>
2018-03-10 10:21:20 -0700

    Added support for critical monitoring intervals

--- Power Source.indigoPlugin/Contents/Server Plugin/plugin.py
+++ Power Source.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -68,16 +68,33 @@ class Plugin(indigo.PluginBase):
 
     #---------------------------------------------------------------------------
     def _runLoopStep(self):
-        # TODO pick updateInterval based on power failure
-
         # devices are updated when added, so we'll start with a sleep
-        updateInterval = int(self.pluginPrefs.get('updateInterval', 5))
-        self.logger.debug('Next update in %d minutes' % updateInterval)
+        updateInterval = self._getCurrentUpdateInterval();
+        self.logger.debug('Next update in %f minutes' % updateInterval)
 
         # sleep for the designated time (convert to seconds)
         self.sleep(updateInterval * 60)
 
         self._updateAllDevices()
+
+    #---------------------------------------------------------------------------
+    def _getCurrentUpdateInterval(self):
+        # we allow floats in case users choose less than 1 minute for updates
+        interval = 0
+
+        # TODO it would be nice if we could just use the information collected
+        # during a device update; the issue would be that users won't always add
+        # the power supply to their list of devices - so we have to call it here
+        power = pmset.getCurrentPowerInfo()
+
+        if (power.isExternal):
+            self.logger.debug('power is on: using standard interval')
+            interval = float(self.pluginPrefs.get('stdUpdateInt', 5))
+        else:
+            self.logger.debug('power is out: using critical interval')
+            interval = float(self.pluginPrefs.get('critUpdateInt', 1))
+
+        return interval
 
     #---------------------------------------------------------------------------
     def _updateAllDevices(self):
